dfa28e0f04a778206afd19f800017a9b190bcbb3
nauva <fadilahaffandi696@gmail.com>
2025-04-17 16:06:09 +0700

    fix import

--- assets/setting-B6l8JBRM.js
+++
@@ -1,1 +0,0 @@
-import{h as t,r as o}from"./index-BPZCxXsg.js";const p=t("pomodoro",()=>{let r=o(25),e=o(5);return{focus:r,rest:e}},{persist:!0});export{p};
---
+++ assets/setting-TM-nGySD.js
@@ -0,0 +1,1 @@
+import{h as t,r as o}from"./index-ByitSgyL.js";const p=t("pomodoro",()=>{let r=o(25),e=o(5);return{focus:r,rest:e}},{persist:!0});export{p};
