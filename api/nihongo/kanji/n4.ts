1d0b3acd3ec8482697b93d5f12124709dc379922
nauva <fadilahaffandi696@gmail.com>
2025-05-02 01:50:43 +0700

    build

---
+++ api/nihongo/kanji/n4.ts
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(`/database/kanji/n4.json`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(404).json({
+      message: 'Data tidak ditemukan'
+    });
+  }
+}
