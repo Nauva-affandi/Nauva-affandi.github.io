cbb6c468ce0ca2ce059862804cbda6373e083ab8
nauva <fadilahaffandi696@gmail.com>
2025-05-03 01:33:19 +0700

    build

--- api/nihongo/kanji/n4.js
+++ api/nihongo/kanji/n4.js
@@ -1,17 +1,21 @@
 import wrapper from './../../_utils/withMiddleWare.js'
 
+import fs from 'fs/promises';
+import path from 'path';
 
 async function handler(req, res) {
   try {
-    const response = await fetch(`/database/nihongo/kanji/n4.json`);
-    const data = await response.json();
+    const filePath = path.join(process.cwd(), 'database/nihongo/kanji/n4.json');
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const data = JSON.parse(fileContent);
     res.status(200).json(data);
   } catch (err) {
     res.status(404).json({
-      message: 'Data tidak ditemukan',
-      typeError: err,
+      message: 'Data tidak ditemukan 5',
+      typeError: err.message,
     });
   }
 }
+
 
 export default wrapper(handler)
